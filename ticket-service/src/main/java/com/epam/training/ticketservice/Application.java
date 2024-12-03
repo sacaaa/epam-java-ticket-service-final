cbacc597d2b0344cb5e27320af2c2baa6a6ccf89
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-12-03 18:30:59 +0100

    edit: spring boot application

--- ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
+++ ticket-service/src/main/java/com/epam/training/ticketservice/Application.java
@@ -1,6 +1,21 @@
 package com.epam.training.ticketservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
     }
 }
