d16c8d5b70fcaa77b22273d84d6aa6b76ad802a9
sacaaa <szekelyhidi.sandor2@gmail.com>
2024-12-04 10:07:40 +0100

    deL: authservice kivezetése

--- ticket-service/src/main/java/com/epam/training/ticketservice/core/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.epam.training.ticketservice.core.service;
-
-import com.epam.training.ticketservice.core.data.Result;
-
-public interface AuthService {
-
-    Result<Void> checkAdminPrivileges();
-
-}
